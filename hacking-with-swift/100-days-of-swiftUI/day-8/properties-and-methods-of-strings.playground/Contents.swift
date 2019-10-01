6e9230e819bd7872b7751f2da8941b4aea3df03a
rafaelviewcontroller <rafaeldelegate@rafaelviewcontrollers-iMac.local>
2019-09-30 22:10:37 -0700

    day 16

---
+++ hacking-with-swift/100-days-of-swiftUI/day-8/properties-and-methods-of-strings.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+//We’ve used lots of strings so far, and it turns out they are structs – they have their own methods and properties we can use to query and manipulate the string.
+//
+//First, let’s create a test string:
+
+let string = "Do or do not, there is no try."
+//You can read the number of characters in a string using its count property:
+//
+print(string.count)
+//They have a hasPrefix() method that returns true if the string starts with specific letters:
+
+print(string.hasPrefix("Do"))
+//You can uppercase a string by calling its uppercased() method:
+
+print(string.uppercased())
+//And you can even have Swift sort the letters of the string into an array:
+
+print(string.sorted())
+//Strings have lots more properties and methods – try typing string. to bring up Xcode’s code completion options.
