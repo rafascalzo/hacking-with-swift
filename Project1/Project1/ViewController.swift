440d9db233ff9a9b131ad143ed2711523913f06b
rafaelviewcontroller <rafaeldelegate@rafaelviewcontrollers-iMac.local>
2019-10-01 13:05:57 -0700

    aleluia

--- Project1/Project1/ViewController.swift
+++ Project1/Project1/ViewController.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
     
     
     @IBOutlet var tableView: UITableView!
+    
     var pictures = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellid")
+        tableView.delegate = self
+        tableView.dataSource = self
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
@@ -28,51 +42,4 @@ class ViewController: UIViewController {
         print(pictures)
         
     }
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(true); tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Picture")
-//
-//    }
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Picture")
-    }
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return pictures.count
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
-        return cell
-    }
-    
-    /*
-     // Override to support rearranging the table view.
-     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-     
-     }
-     */
-    
-    /*
-     // Override to support conditional rearranging of the table view.
-     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-     // Return false if you do not want the item to be re-orderable.
-     return true
-     }
-     */
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
-    
-    
-    
 }
-
