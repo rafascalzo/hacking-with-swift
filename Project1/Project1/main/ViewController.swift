66c6d936fb65863e135dd380857e64c7665f0edd
rafaelviewcontroller <rafaeldelegate@rafaelviewcontrollers-iMac.local>
2019-10-01 19:50:44 -0700

    day 17

---
+++ Project1/Project1/main/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by rafaelviewcontroller on 10/1/19.
+//  Copyright © 2019 rafaelviewcontroller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+ 
+    @IBOutlet var tableView: UITableView!
+    
+    var pictures = [String]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cellid")
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pictures.append(item)
+            }
+        }
+        
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellid", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        cell.accessoryType = .disclosureIndicator
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        let controller = DetailViewController()
+        controller.selectedImage = pictures[indexPath.row]
+        if let navigation = navigationController {
+            navigation.pushViewController(controller, animated: true)
+        }
+        
+    }
+}
